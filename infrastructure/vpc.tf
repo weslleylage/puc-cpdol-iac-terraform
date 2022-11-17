edb93b979a5fed02885271072e5e38a78446e8df
Weslley Lage <weslley.lage@solides.com.br>
2022-11-17 16:12:48 -0300

    ajustes

--- infrastructure/vpc.tf
+++ infrastructure/vpc.tf
@@ -1,5 +1,6 @@
 resource "aws_vpc" "this" {
   cidr_block = "10.0.0.0/16"
+  enable_dns_hostnames = true
   tags = merge(
     {
       Name = "${local.prefix}-vpc"
--- scripts/userdata-ec2.sh
+++ scripts/userdata-ec2.sh
@@ -2,8 +2,8 @@
 
 sudo apt-get update
 sudo apt-get install -y curl wget nfs-common
-sudo mkdir /app/puc-app-cpdol-iac
-sudo mount -t nfs4 -o nfsvers=4.1,rsize=1048576,wsize=1048576,hard,timeo=600,retrans=2,noresvport ${aws_efs_file_system.this.dns_name}:/ /app/puc-app-cpdol-iac
+sudo mkdir /app
+sudo mount -t nfs4 -o nfsvers=4.1,rsize=1048576,wsize=1048576,hard,timeo=600,retrans=2,noresvport ${aws_efs_file_system.this.dns_name}:/ /app
 curl -fsSl https://get.docker.com | bash
 sudo docker pull weslleylage/puc-app-cpdol-iac
-sudo docker run -d -v /usr/share/nginx:/app/puc-app-cpdol-iac --name puc-app-cpdol-iac -p 80:80 weslleylage/puc-app-cpdol-iac
+sudo docker run -d -v /usr/share/nginx/html:/app --name puc-app-cpdol-iac -p 8080:80 weslleylage/puc-app-cpdol-iac
